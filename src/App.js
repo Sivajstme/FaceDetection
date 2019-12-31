cb214aac18494514b5ff82293108bfac4b50093a
siva <shivajstme@gmail.com>
2019-12-31 13:11:17 +1100

    Signin and faceRecognition

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Logo from './components/Logo/Logo';
 import Particles from "react-particles-js";
+import API from './components/API/Clarifai'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
 
 const particlesOption = {
   particles: {
@@ -15,6 +18,14 @@ const particlesOption = {
         value_area : 600
       }
     }
+  },
+  'interactivity':{
+    'events' : {
+      'onhover'  :{
+        'enable' : true,
+        'mode' : 'repulse'
+      }
+    }
   }
 }
 
@@ -23,15 +34,62 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      input : ''
+      input : '',
+      imageUrl : '',
+      box : {}
     }
   }
 
-  onInputChange = (e) =>{
-    console.log(e.target.value);
+  calculateFaceLocation = (dataInput) => {
+    console.log('testes');
+    const clarifaiFace = dataInput.outputs[0].data.regions[0].region_info.bounding_box;
+    //console.log(clarifaiFace);
+    const img = document.getElementById("inputImage");
+    //console.log(img)
+    let width = Number(img.width);
+    let height = Number(img.height);
+    console.log(height, width); //Height = 441;
+                                //width = 500;
+  /**
+    top_row: 0.15593769,
+    left_col: 0.32949424,
+    bottom_row: 0.6229454,
+    right_col: 0.63938797 width -
+   */
+  return {
+    left_col: width * clarifaiFace.left_col,
+    top_row: height * clarifaiFace.top_row,
+    right_col: width - clarifaiFace.right_col * width,
+    bottom_row: height - clarifaiFace.bottom_row * height
   }
-  onButtonSubmit = ()=>{
-    console.log('Click')
+  
+}
+
+  borderBox = (border) =>{
+    //console.log(border);
+    this.setState({box : border});
+  }
+
+  onInputChange = (e) =>{
+    this.setState({input : e.target.value})
+  }
+  onButtonSubmit = () =>{
+    console.log('Click');
+    this.setState({imageUrl : this.state.input});
+    API.models.predict("a403429f2ddf4b49b307e318f00e528b", this.state.input)
+      .then(response => this.borderBox(this.calculateFaceLocation(response)))
+              .catch(err => console.log(err));
+                  //console.log(response)  
+                  //console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    
+    //return this.borderBox(this.calculateFaceLocation(response));
+
+        // top_row: 0.15593769,
+        // left_col: 0.32949424,
+        // bottom_row: 0.6229454,
+        // right_col: 0.63938797
+        
+              
   }
   render() {
     return(
@@ -39,12 +97,13 @@ class App extends Component {
           <Particles className = 'particles'
             params={particlesOption}/>
         <Navigation />
+        <Signin />
         <Logo />
         <Rank />
         <ImageLinkForm 
             onInputChange = {this.onInputChange} 
             onButtonSubmit = {this.onButtonSubmit}/>
-        {/* <FaceRecognition /> */}
+        <FaceRecognition imageBorder = {this.state.box} imageSource= {this.state.imageUrl}/>
       </div>
     )
   };
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import './FaceRecognition.css'
 
 const FaceRecognition = (props) =>{
     return (
             <div className='center ma'>
                 <div className='absolute mt2'>
-                    <img src={props.imageSource} alt = '' width ='500px' height='auto'/>
+                    <img id='inputImage' src={props.imageSource} alt = '' width ='500px' height='auto'/>
+                <div className='bounding-box' style={{
+                    top: props.imageBorder.top_row, left: props.imageBorder.left_col,
+                                                        right: props.imageBorder.right_col, bottom: props.imageBorder.bottom_row
+                                                    
+                                                    }}></div>
                 </div>
             </div>
-    )
+            /*top_row: 0.15593769,
+            props.imageSource.left_col: 0.32949424,
+            props.imageSource.bottom_row: 0.6229454,
+             props.imageSource.right_col: */
+            ) 
 }
 
 export default FaceRecognition;
