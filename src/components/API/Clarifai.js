8b8798fb773af733b6196c2e03ac82e382bb9db1
shivajstme <37784780+Sivajstme@users.noreply.github.com>
2020-01-02 18:48:48 +1100

    Delete Clarifai.js

--- src/components/API/Clarifai.js
+++
@@ -1,8 +0,0 @@
-import Clarifai from 'clarifai';
-
-// const API = new Clarifai.APP({
-//     apikey: '3c10409212944e42865dfa38fdd16256'
-// })
-const api = new Clarifai.App({ apiKey: "3c10409212944e42865dfa38fdd1625" });
-
-export default api;
