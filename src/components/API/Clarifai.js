62f9b49b2b19fd10837d8ef97401a6dd92076b92
shivajstme <37784780+Sivajstme@users.noreply.github.com>
2020-01-04 10:05:24 +1100

    Update Clarifai.js

--- src/components/API/Clarifai.js
+++ src/components/API/Clarifai.js
@@ -1,8 +1,8 @@
 import Clarifai from 'clarifai';
 
 // const API = new Clarifai.APP({
-//     apikey: '3c10409212944e42865dfa38fdd16256'
+//     apikey: '3c10409212944e42865dfa38fdd16'
 // })
-const api = new Clarifai.App({ apiKey: "3c10409212944e42865dfa38fdd16256" });
+const api = new Clarifai.App({ apiKey: "3c10409212944e42865dfa38fdd16" });
 
 export default api;
